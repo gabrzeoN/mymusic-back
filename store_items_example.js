b79770eac1459fa5f4683c71c03efc6e1347e971
Gabriel Cari <g.cari@hotmail.com>
2022-05-15 23:00:26 -0300

    Feat: database example.

---
+++ store_items_example.js
@@ -0,0 +1,142 @@
+//  -------------------- Database example ----------------------
+//  types: eletric-guitar, accoustic-guitar, bass, guitar-amp, bass-amp
+
+const database_example = [
+    {
+        "name": "AMERICAN PROFESSIONAL II STRATOCASTER®",
+        "image": "https://i0.wp.com/guitarload.com.br/wp-content/uploads/2018/04/fender-strat-tele-hybrid-nova-guitarra-1.png?fit=800%2C260&ssl=1",
+        "description": "The American Professional II Stratocaster® draws from more than sixty years of innovation, inspiration and evolution to meet the demands of today’s working player. \nOur popular Deep “C” neck now sports smooth rolled fingerboard edges, a “Super-Natural” satin finish and a newly sculpted neck heel for a supremely comfortable feel and easy access to the upper register. New V-Mod II Stratocaster single-coil pickups are more articulate than ever while retaining bell-like chime and warmth. An upgraded 2-point tremolo with a cold-rolled steel block increases sustain, clarity and high-end sparkle. \nThe American Pro II Stratocaster delivers instant familiarity and sonic versatility you’ll feel and hear right away, with broad ranging improvements that add up to nothing less than a new standard for professional instruments.",
+        "type": "eletric-guitar",
+        "price": 1500
+    },
+    {
+        "name": "AMERICAN PROFESSIONAL II STRATOCASTER®",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10001/0113900755_fen_ins_frt_1_rr.jpg",
+        "description": "The American Professional II Stratocaster® draws from more than sixty years of innovation, inspiration and evolution to meet the demands of today’s working player. Our popular Deep “C” neck now sports smooth rolled fingerboard edges, a “Super-Natural” satin finish and a newly sculpted neck heel for a supremely comfortable feel and easy access to the upper register. New V-Mod II Stratocaster single-coil pickups are more articulate than ever while retaining bell-like chime and warmth. An upgraded 2-point tremolo with a cold-rolled steel block increases sustain, clarity and high-end sparkle. The American Pro II Stratocaster delivers instant familiarity and sonic versatility you’ll feel and hear right away, with broad ranging improvements that add up to nothing less than a new standard for professional instruments.",
+        "type": "eletric-guitar",
+        "price": 1500
+    },
+    {
+        "name": "AMERICAN PROFESSIONAL II STRATOCASTER® LEFT-HAND",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10001/0113932718_fen_ins_frt_1_rl.jpg",
+        "description": "The American Professional II Stratocaster® Left-Hand draws from more than sixty years of innovation, inspiration and evolution to meet the demands of today’s working player. Our popular Deep “C” neck now sports smooth rolled fingerboard edges, a “Super-Natural” satin finish and a newly sculpted neck heel for a supremely comfortable feel and easy access to the upper register. New V-Mod II Stratocaster single-coil pickups are more articulate than ever while retaining bell-like chime and warmth. An upgraded 2-point tremolo with a cold-rolled steel block increases sustain, clarity and high-end sparkle. The American Pro II Stratocaster Left-Hand delivers instant familiarity and sonic versatility you’ll feel and hear right away, with broad ranging improvements that add up to nothing less than a new standard for professional instruments.",
+        "type": "eletric-guitar",
+        "price": 1500
+    },
+    {
+        "name": "PLAYER PLUS NASHVILLE TELECASTER®",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10001/0147342350_fen_ins_frt_1_rr.jpg",
+        "description": "Fusing classic Fender design with player-centric features and exciting new finishes, the Player Plus Nashville Telecaster® delivers superb playability and unmistakable style. Powered by a set of Player Plus Noiseless pickups, the Player Plus Nashville Tele® delivers warm, sweet Tele® twang – as well as Strat® in-between tones – without hum. A push-pull switch on the tone control engages the neck pickup in switch positions 1 & 2 for two additional tones. The silky satin Modern “C” Player Plus Tele® neck fits your hand like a glove, with smooth rolled edges for supreme comfort. The 12” radius fingerboard and 22 medium jumbo frets facilitate fluid leads and choke free bends. A modern 6-saddle Tele bridge with block steel saddles adds a touch of brightness while providing precise intonation and the locking tuners provide rock-solid tuning and make string changes quick and easy. With classic Fender style, advanced features and stunning new finishes, the Player Plus Nashville Telecaster is the perfect tool to spark your creativity and stand out from the crowd.",
+        "type": "eletric-guitar",
+        "price": 1500
+    },
+    {
+        "name": "AMERICAN PROFESSIONAL II TELECASTER®",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10001/0113942700_fen_ins_frt_1_rr.jpg",
+        "description": "The American Professional II Telecaster® draws from more than seventy years of innovation, inspiration and evolution to meet the demands of today’s working player. Our popular Deep 'C' neck now sports smooth rolled fingerboard edges, a “Super-Natural” satin finish and a newly sculpted neck heel for a supremely comfortable feel and easy access to the upper register. New V-Mod II Telecaster single-coil pickups are more articulate than ever while delivering the twang, snap and snarl that made the Tele famous. The new top-load/string-through bridge with compensated “bullet” saddles is our most comfortable, flexible Tele bridge yet – retaining classic brass-saddle tone and providing excellent intonation and flexible setup options, allowing you to tailor the tension and tone of each string to your liking. The American Pro II Telecaster delivers instant familiarity and sonic versatility you’ll feel and hear right away, with broad ranging improvements that add up to nothing less than a new standard for professional instruments.",
+        "type": "eletric-guitar",
+        "price": 1500
+    },
+    {
+        "name": "AMERICAN PROFESSIONAL II TELECASTER® LEFT-HAND",
+        "image": "https://www.fmicassets.com/Damroot/ZoomJpg/10001/0113952718_fen_ins_frt_1_rl.jpg",
+        "description": "The American Professional II Telecaster® Left-Hand draws from more than seventy years of innovation, inspiration and evolution to meet the demands of today’s working player. Our popular Deep “C” neck now sports smooth rolled fingerboard edges, a “Super-Natural” satin finish and a newly sculpted neck heel for a supremely comfortable feel and easy access to the upper register. New V-Mod II Telecaster single-coil pickups are more articulate than ever while delivering the twang, snap and snarl that made the Tele famous. The new top-load/string-through bridge with compensated “bullet” saddles is our most comfortable, flexible Tele bridge yet – retaining classic brass-saddle tone and providing excellent intonation and flexible setup options, allowing you to tailor the tension and tone of each string to your liking. The American Pro II Telecaster Left-Hand delivers instant familiarity and sonic versatility you’ll feel and hear right away, with broad ranging improvements that add up to nothing less than a new standard for professional instruments.",
+        "type": "eletric-guitar",
+        "price": 1500
+    },
+    {
+        "name": "CONTEMPORARY JAGUAR® HH ST",
+        "image": "https://www.fmicassets.com/Damroot/ZoomJpg/10001/0370350544_sqr_ins_frt_1_rr.jpg",
+        "description": "The Squier® Contemporary Jaguar® HH ST brings modern features and bold aesthetics to an iconic Fender platform to satisfy today’s most discerning and daring players. At the heart of this guitar is a pair of Squier SQR™ Atomic™ humbucking pickups with coil-split and series/parallel switching on the upper control plate for a wide range of genre-defying sonic settings. The roasted maple neck was chosen for its optimal tone and stability, and a sculpted heel design allows for improved access all the way to the 22nd fret. High-performance components round out the feature list and include a fully adjustable bridge with stop tailpiece (ST) for rock-solid string feel, and a set of sealed-gear tuning machines with split shafts for smooth tuning action and easy restringing. Eye-catching styling cues include lustrous chrome hardware and a sleek painted headstock with chrome logos for a premium look.",
+        "type": "eletric-guitar",
+        "price": 1500
+    },
+    {
+        "name": "JIM ROOT JAZZMASTER®",
+        "image": "https://www.fmicassets.com/Damroot/ZoomJpg/10002/0115300706_gtr_frt_001_rr.jpg",
+        "description": "Stark, dark and menacing, the crushing Jim Root Jazzmaster guitar has got to be the most distinctively minimalist version of the instrument ever devised in the model's entire half-century history. At the behest of the towering Slipknot guitarist, gone are the dual tone circuits and barrage of controls. Gone are the fret position markers and enormous chrome bridge. Gone, in fact, is pretty much everything typical of a Jazzmaster, replaced only by fearsome EMG® humbucking pickups with brushed nickel covers, single three-way switch and single volume knob, a hardtail bridge and little else.",
+        "type": "eletric-guitar",
+        "price": 1500
+    },
+    {
+        "name": "JIM ROOT JAZZMASTER® V4",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10001/0145301780_gtr_frt_001_rr.jpg",
+        "description": "For heavy, molten metal riffage, the Jim Root Jazzmaster® V4 delivers crushing detuned tone with a bold new look. We worked closely with the towering Slipknot guitarist to create a brutal-sounding signature Jazzmaster model that complements his heavy playing style—right down to its signature Daemonum™ open-coil EMG® active pickups, shred-worthy 12” radius fingerboard with jumbo frets, sparse control layout and more. The Jim Root Jazzmaster II dispenses with frivolities such as a vibrato, rhythm circuit, tone control – only crucial essentials remain: a volume control, a 3-way switch and a hardtail bridge, making this guitar a single-purpose weapon of mass destruction. Featuring a mahogany slab body for crushing lows and mids, brilliant Polar White satin finish, maple neck with bound ebony fingerboard and pearloid block inlays, this devastating machine delivers brutal tone with a striking new look.",
+        "type": "eletric-guitar",
+        "price": 1500
+    },
+    {
+        "name": "PLAYER JAGUAR®",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10002/0146303513_gtr_frt_001_rr.jpg",
+        "description": "Sleek and stylish and with a voice that’s a modern twist on our classic sound, the Player Jaguar has a uniquely slinky playing feel. It encourages you to play differently, to reach for new chords and melodic twists and to explore new sounds and tonalities. Decked out with spotlight-ready chrome accents, it has the authentic sound, features and feel that’s inspired countless artists to write new songs and create new genres of music.",
+        "type": "eletric-guitar",
+        "price": 1500
+    },
+    {
+        "name": "KURT COBAIN JAGUAR® LEFT-HAND",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10002/0143021700_gtr_frt_001_rl.jpg",
+        "description": "Return to the dark intensity and huge guitar sounds of the grunge rock movement with the Kurt Cobain Jaguar, a new-looking version of the battered and highly unusual 1965 Jaguar that Cobain wielded during the heady early-'90s era when Nirvana ruled rock and led a musically stunning and culturally subversive movement. It authentically evokes the modified left-handed Jaguar that Cobain found secondhand and came to prize shortly after the recording of massive 1991 album Nevermind, complete with DiMarzio® humbucking pickups, special switching, exclusive in-case extras and more. Available in right- and left-handed versions.",
+        "type": "eletric-guitar",
+        "price": 1500
+    },
+
+    {
+        "name": "ACOUSTASONIC® PLAYER TELECASTER®",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10001/0972213239_fen_ins_frt_1_rr.jpg",
+        "description": "The evolution of Fender’s trailblazing Acoustasonic platform continues with the introduction of the Acoustasonic Player Telecaster. This acoustic-electric guitar offers its own set of six voices that showcases its unique personality. A streamlined 3-way switch allows for shape-shifting between acoustic and electric tones. With the Acoustasonic Player Telecaster, this platform has been refined to offer a versatile, more accessible tool for the modern musician.",
+        "type": "accoustic-guitar",
+        "price": 1500
+    },
+    {
+        "name": "AMERICAN ACOUSTASONIC® STRATOCASTER®",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10001/0972023200_gtr_frt_001_rr.jpg",
+        "description": "The American Acoustasonic™ Stratocaster® continues to embody the spirit of purposeful innovation that drives Fender guitars. The power of the Fender and Fishman®-designed Acoustic Engine is sure to deliver true inspiration. From acoustic shapeshifting to electric rhythm tones, this extremely versatile guitar creates a brand-new lane on the sonic highway.",
+        "type": "accoustic-guitar",
+        "price": 1500
+    },
+
+    {
+        "name": "NATE MENDEL P BASS®",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10001/0142500309_gtr_frt_001_rr.jpg",
+        "description": "Throughout a long career that started with hardcore punk and flourished first with Sunny Day Real Estate and then the Foo Fighters, there has really only been one main instrument for melodic yet hard-hitting bassist Nate Mendel—his 1971 Precision Bass®. Fender now brings you the Nate Mendel P Bass, modeled after his beloved, battle-hardened instrument and sharing its subtly distinctive features, including the special body contours, extra-slim neck, lightly worn finish, powerful Seymour Duncan® pickup and more.",
+        "type": "bass",
+        "price": 1500
+    },
+    {
+        "name": "DUFF MCKAGAN PRECISION BASS®",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10001/0146500323_gtr_frt_001_rr.jpg",
+        "description": "Designed for the man who has held down the low end for Guns N' Roses, Velvet Revolver, Jane's Addiction and many others, the Duff McKagan Precision Bass is rock 'n' roll. Based on the Pearl White instrument McKagan has played since the first GNR album, the Duff McKagan Signature model is a distinctive, sleek and full-sounding reissue of the '80s-era Jazz Bass® Special, which combined a Precision Bass body with a Jazz Bass neck and pickups from both instruments.",
+        "type": "bass",
+        "price": 1500
+    },
+
+    {
+        "name": "'68 CUSTOM VIBROLUX® REVERB",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10007/2275000000_amp_frt_001_nr.jpg",
+        "description": "The guitarist with an eye and ear for late-'60s Fender silver panel style will love the '68 Custom Vibrolux Reverb, which honors the classic look and great sound of that era's amps. The moderately powered Vibrolux Reverb received a fresh new face in 1968—a silver-and-turquoise front panel with aluminum grille cloth trim, with tone, reverb and tremolo that was still pure Fender. That's all available in the 2x10\" '68 Custom Vibrolux Reverb, too, which also has modified all-tube circuitry, vintage channel for traditional silver face tone, custom channel with Bassman® tone stack, and reduced negative feedback for greater touch sensitivity and quicker gain onset.",
+        "type": "guitar-amp",
+        "price": 1500
+    },
+    {
+        "name": "'57 CUSTOM DELUXE™",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10001/8150500100_amp_frt_001_nr.jpg",
+        "description": "The handwired '57 Custom Deluxe is a bit of electronic wizardry—an extraordinary amplifier with stunning clean tone and the complex, organic overdrive guitarists crave. Based on the original highly sought-after collector's piece, this two-channel combo sings and snarls with the authentic Fender sound that birthed rock 'n' roll.",
+        "type": "guitar-amp",
+        "price": 1500
+    },
+
+    {
+        "name": "SUPER BASSMAN®",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10001/2249000000_fen_amp_frt_1_nr.jpg",
+        "description": "The flagship of the Fender bass amp lineup is the mighty Super Bassman. One of the most powerful and versatile Fender bass amps ever made, it's a stadium-level 300-watt tube head designed for the biggest shows and the largest stages, with enormous Fender bass sound and sparkling vintage-style Fender looks. Equally effective in the studio, its dual-channel design combines modern technology and an array of innovative features on the front and rear panels with classic Fender mid-'60s style. The maximum in pure Fender bass power and performance.",
+        "type": "bass-amp",
+        "price": 1500
+    },
+    {
+        "name": "BASSMAN® 410 NEO ENCLOSURE",
+        "image": "https://www.fmicassets.com/Damroot/LgJpg/10007/2249400000_amp_frt_001_nr.jpg",
+        "description": "For bassists everywhere who must have the best in modern Bassman style and performance, the Bassman 410 Neo cabinet delivers the full bass sound you'd expect from a 4x10\" enclosure, with sparkling vintage Fender looks and a remarkably lightweight design ideal for shows, venues and studios of any size.",
+        "type": "bass-amp",
+        "price": 1500
+    }
+]
